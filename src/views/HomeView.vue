eb4b66c6df2c52b8c8d2f0dc0283ca6ab4aa11b8
mich <danielrodrigocm@hotmail.com>
2022-09-12 14:59:38 -0500

    Seccion 7 terminada

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,15 @@
-<template>
-  <div class="home">
-    
-  </div>
-</template>
-
 <script setup>
-</script>
+  import { storeToRefs } from "pinia";
+  import { useCounterStore } from "@/store/counter";
+  
+  const useCounter = useCounterStore();
+  
+  const { count, double } = storeToRefs(useCounter);
+  const { increment } = useCounter;
+  </script>
+  
+  <template>
+      <h1>Home Counter: {{ count }}</h1>
+      <h2>Double: {{ double }}</h2>
+      <button @click="increment">Increment</button>
+  </template>
